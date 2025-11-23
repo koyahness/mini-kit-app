2bf6b995f7316a679ff97e2fd0906d026c3460a1
koyahness <cryptomakoya@gmail.com>
2025-11-23 15:07:45 +0200

    wasm_cosmos

---
+++ koyah_wasm_test_template/src/bin/schema.rs
@@ -0,0 +1,11 @@
+use cosmwasm_schema::write_api;
+
+use koyah_wasm_test_template::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecuteMsg,
+        query: QueryMsg,
+    }
+}
